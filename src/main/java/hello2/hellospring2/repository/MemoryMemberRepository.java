c3398dfbba3c382dc9f4344ae072468aaca4439a
yujin cha <white8769@naver.com>
2023-02-16 14:38:33 +0900

    Initial commit

---
+++ src/main/java/hello2/hellospring2/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello2.hellospring2.repository;
+
+import hello2.hellospring2.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long,Member>store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);//store에 저장 Map에 저장됨
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
---
+++ src/main/java/hello2/hellospring2/service/MemberService.java
@@ -0,0 +1,43 @@
+package hello2.hellospring2.service;
+
+import hello2.hellospring2.domain.Member;
+import hello2.hellospring2.repository.MemberRepository;
+import hello2.hellospring2.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     *회원가입
+     */
+        public Long join(Member member) {
+            //같은 이름이 있는 중복회원X
+            validateEuplicateMember(member);//중복회원 검증
+
+            memberRepository.save(member);
+            return member.getId();
+        }
+
+    private void validateEuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())//단축키 ctrl + alt + v 변수추출
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다."); //메소드 만들기 ctrl+alt+shift+T한 후,method 검색
+        });
+    }
+
+    /**
+     *전체 회원 조회
+     */
+
+    public List<Member> findMembers(){
+            return memberRepository.findAll();
+    }
+    public Optional<Member>findOne(Long memberId){
+            return memberRepository.findById(memberId);
+    }
+
+}
